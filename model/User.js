14a94fba92bc1d4771117bb980a5ed48d2721d09
burhanali2812 <syedburhanali830@gmail.com>
2025-05-23 22:37:36 +0500

    set User to Admin

--- model/User.js
+++ model/User.js
@@ -7,11 +7,11 @@ const userSchema = new mongoose.Schema({
     phone: { type: String, required: true },
     cnic: { type: String, required: true, unique: true , sparse: true},
     address: { type: String, required: true },
-    isVerified: { type: String, required: true, default: "requested" },
+    isVerified: { type: String, required: true, default: "accepted" },
     profileImage : { type: String, required: true },
     frontCnic : { type: String, required: true },
     backCnic : { type: String, required: true },
-    role: { type: String, required: true, default: "user" },
+    role: { type: String, required: true, default: "admin" },
     createdAt: { type: Date, default: Date.now }
   });
   
