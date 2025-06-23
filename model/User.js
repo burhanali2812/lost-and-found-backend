979ca1e3b83d6ba4a420ddf041b0ef0949cdfe39
burhanali2812 <syedburhanali830@gmail.com>
2025-06-23 15:39:00 +0500

    update User Schema

--- model/User.js
+++ model/User.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true , sparse: true},
-    password: { type: String, required: true },
+    password: { type: String, required: true ,  default: null},
     phone: { type: String, required: true },
     cnic: { type: String, required: true, unique: true , sparse: true},
     address: { type: String, required: true },
     isVerified: { type: String, required: true, default: "requested" },
-    profileImage : { type: String, required: true },
-    frontCnic : { type: String, required: true },
-    backCnic : { type: String, required: true },
+    profileImage : { type: String, required: true ,  default: null},
+    frontCnic : { type: String, required: true ,  default: null},
+    backCnic : { type: String, required: true ,  default: null},
     role: { type: String, required: true, default: "user" },
     createdAt: { type: Date, default: Date.now }
   });
